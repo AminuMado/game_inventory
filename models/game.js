88f638fb57ef3527caadcd7ccd944805e19f1a66
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-12 21:44:16 +0100

    initial commit

---
+++ models/game.js
@@ -0,0 +1,21 @@
+//Requre mongoose
+const mongoose = require("mongoose");
+
+//Define a schema
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+  title: { type: string, required: true, maxLength: 100, minLength: 3 },
+  pictures: [{ type: string, required: false }],
+  releaseDate: {
+    type: Number,
+    required: true,
+    min: [1958, "Really? Earlier than Tennis for Two?"],
+    max: 2099,
+  },
+  director: { type: String, required: true },
+  summary: { type: String, required: false },
+  platform: [{ type: Schema.Types.ObjectId, ref: "Platform", required: true }],
+  developer: { type: Schema.Types.ObjectId, ref: "Developer", required: true },
+  genre: { type: Schema.Types.ObjectId, ref: "Genre", required: false },
+});
