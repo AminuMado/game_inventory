f4afcaaa39c5603254b826f5ce726e38f2341021
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-12 21:44:28 +0100

    initial commit

---
+++ models/reviews.js
@@ -0,0 +1,18 @@
+// Require mongoose
+const mongoose = require("mongoose");
+
+//Define Schema
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+  game: { type: Schema.Types.ObjectId, ref: "Game", required: true },
+  sourceSite: { type: String, required: true },
+  content: { type: String, required: false },
+  rating: { type: String, required: true },
+  link: { type: String, required: false },
+});
+
+//Virtual for review's URL
+ReviewSchema.virtual("url").get(function () {
+  return `/review/${this._id}`;
+});
