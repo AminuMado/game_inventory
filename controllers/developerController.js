1f2384c2158fd0dcf8ea5cdb1c6a5889e7ba27f6
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-13 03:44:13 +0100

    add boilerplate for route function handlers

---
+++ controllers/developerController.js
@@ -0,0 +1,39 @@
+// Display list of All Developers
+exports.developer_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Developer List ");
+};
+
+// Display Detail page for a Specific Developer
+exports.developer_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Developer Detail: ${req.params.id}`);
+};
+
+// Display Create Form on GET
+exports.developer_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Developer Create GET");
+};
+
+// Handle Create on POST
+exports.developer_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Developer Create POST");
+};
+
+// Display Delete Form on GET
+exports.developer_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Developer Delete GET");
+};
+
+// Handle Delete on POST
+exports.developer_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Developer Delete POST");
+};
+
+// Display Update Form on GET
+exports.developer_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Developer Update GET");
+};
+
+// Handle Update on POST
+exports.developer_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: DeveloperUpdate POST");
+};
