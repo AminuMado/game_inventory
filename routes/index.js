8b6d681a5c46e6218b0e48a927cdb4714f0bb020
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-13 03:18:10 +0100

    add review routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var router = express.Router();
+
 // Require Controller Modules
 const developer_controller = require("../controllers/developerController");
 const game_controller = require("../controllers/gameController");
 const genre_controller = require("../controllers/genreController");
 const platform_controller = require("../controllers/platformController");
-const reviews_controller = require("../controllers/reviewsController");
+const review_controller = require("../controllers/reviewController");
 const genre = require("../models/genre");
 
 /* GET home page. */
@@ -71,7 +72,7 @@ router.get("/developer/:id", developer_controller.developer_detail);
 //Get Request for all Developers
 router.get("/developers", developer_controller.developer_list);
 
-// Platform Routes
+// PLATFORM ROUTES
 
 // Get Request to Create a Platform
 router.get("/platform/create", platform_controller.platform_create_get);
@@ -93,7 +94,8 @@ router.get("/platform/:id", platform_controller.platform_detail);
 // Get Request for all Platforms
 router.get("/platforms", platform_controller.platform_list);
 
-// Genre Routes
+// GENRE ROUTES
+
 // Get Request to Create a Genre
 router.get("/genre/create", genre_controller.genre_create_get);
 // Post Request to Create a Genre
@@ -113,6 +115,28 @@ router.post("/genre/:id/update", genre_controller.genre_update_post);
 router.get("/genre/:id", genre_controller.genre_detail);
 // Get Request for all Genres
 router.get("/genres", genre_controller.genre_list);
-// Review Routes
 
+// REVIEW ROUTES
+
+// Get Request to Create a Review
+router.get("/review/create", review_controller.review_create_get);
+// Post Request to Create a Review
+router.post("/review/create", review_controller.review_create_post);
+
+// Get Request to Delete a Review
+router.get("/review/:id/delete", review_controller.review_delete_get);
+// Post Request to Delete a Review
+router.post("/review/:id/delete", review_controller.review_delete_post);
+
+// Get Request to Update a Review
+router.get("/review/:id/update", review_controller.review_update_get);
+// Post Request to Update a Revie
+router.post("/review/:id/update", review_controller.review_update_post);
+
+// Get Request for one Review
+router.get("/review/:id", review_controller.review_detail);
+// Get Request for all Reviews
+router.get("/reviews", review_controller.review_list);
+
+// Export Module
 module.exports = router;
