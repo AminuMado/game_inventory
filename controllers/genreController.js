a9a095a58a1c29dce2c8f76922de8e3e101698f3
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-13 04:01:44 +0100

    add boilerplate for routes handler functions

---
+++ controllers/genreController.js
@@ -0,0 +1,38 @@
+// Display list of All Genres
+exports.genre_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Genre List");
+};
+
+// Display Detail Page for a Specific Genre
+exports.genre_detail = (req, res) => {
+  res.send("NOT IMPLEMENTED: Genre Detail " + req.params.id);
+};
+
+// Display Create Form on GET
+exports.genre_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Genre Create GET");
+};
+
+// Handle Create  on POST
+exports.genre_create_post = (req, res) => {
+  res.send("NOT IMPLMENTED: Genre Create POST");
+};
+
+// Display Delete Form on Get
+exports.genre_delete_get = (req, res) => {
+  res.send("NOT IMPLMENTED: Genre Delete GET");
+};
+
+// Handle Delete on POST
+exports.genre_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Genre Delete POST");
+};
+
+// Display Update Form on GET
+exports.genre_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Genre Update GET");
+};
+// Handle Update on POST
+exports.genre_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Genre Update POST");
+};
