356d8c809de30ac88d37d4d2a3245d1eca03eaa5
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-13 04:02:05 +0100

    add boilderplate for routes handler function

---
+++ controllers/platformController.js
@@ -0,0 +1,36 @@
+// Display List of all Platforms
+exports.platform_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platform List");
+};
+
+// Display a Detail Page for a Specific Platform
+exports.platform_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Platform Detail ${req.params.id}`);
+};
+
+// Display Create Form on GET
+exports.platform_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platform Create GET");
+};
+// Handle Create on POST
+exports.platform_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platform Create POST");
+};
+
+// Display Delete Form on GET
+exports.platform_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platfrom Delete GET");
+};
+// Handle Delete on POST
+exports.platform_delete_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platform Delete POST");
+};
+
+// Display Update Form on GET
+exports.platform_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platform Update GET");
+};
+// Handle Update on POST
+exports.platform_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Platform Update POST");
+};
