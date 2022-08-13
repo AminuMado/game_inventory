ddad117ea4dfd233671280daf827a7429cbef075
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-13 04:17:26 +0100

    add boilerplate to route handler functions

---
+++ controllers/reviewController.js
@@ -0,0 +1,37 @@
+// Display list of All Review
+exports.review_list = (req, res) => {
+  res.send("NOT IMPLEMENTED: Review List");
+};
+
+// Display Details for a Specific Review
+exports.review_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Review Detail ${req.params.id}`);
+};
+
+// Display Create Form on GET
+exports.review_create_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Review Create GET");
+};
+// Handle Create on POST
+exports.review_create_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Review Create POST");
+};
+
+// Display Delete Form on GET
+exports.review_delete_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Review Delete GET");
+};
+// Handle Delete on POST
+exports.review_delete_post = (req, res) => {
+  res.send("NOT IMPLMENTED:Review Delete POST");
+};
+
+// Display Update Form on GET
+exports.review_update_get = (req, res) => {
+  res.send("NOT IMPLEMENTED: Review Update GET");
+};
+
+// Handle Update on POST
+exports.review_update_post = (req, res) => {
+  res.send("NOT IMPLEMENTED: Review Update POST");
+};
