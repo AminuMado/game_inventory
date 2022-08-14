9df312f715027075c0f77a93eaf1d1d854e5102c
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-14 16:03:26 +0100

    initial commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,3 @@
-console.log("WE in?");
 const bonfireLogo = document.querySelector(".bonfire_logo");
 const overlay = document.querySelector(".overlay");
 const sideBar = document.querySelector(".sidebar_container");
@@ -7,5 +6,4 @@ function handleClick(e) {
   overlay.classList.toggle("active");
   sideBar.classList.toggle("active");
 }
-console.log(bonfireLogo);
 bonfireLogo.addEventListener("click", handleClick);
