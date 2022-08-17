8beb5bc9901a8f881f0a2c5cc746c95d6a48eeb9
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-17 21:29:51 +0100

    add genre details page logic

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,8 @@
 //require the genre model
 const Genre = require("../models/genre");
+const async = require("async");
+const Game = require("../models/game");
+
 // Display list of All Genres
 exports.genre_list = (req, res) => {
   Genre.find().exec((err, list_genre) => {
@@ -12,8 +15,39 @@ exports.genre_list = (req, res) => {
 };
 
 // Display Detail Page for a Specific Genre
-exports.genre_detail = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre Detail " + req.params.id);
+exports.genre_detail = (req, res, next) => {
+  // So basically we need to find the games associated with a specific genre
+  // So we'd need an async process and also query the game details associated with
+  // the game that has the same id as the id as specified in the model
+  // Display detail page for a specific Genre.
+  // Display detail page for a specific Genre.
+  async.parallel(
+    {
+      genre: function (callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_games: function (callback) {
+        Game.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_games: results.genre_games,
+      });
+    }
+  );
 };
 
 // Display Create Form on GET
