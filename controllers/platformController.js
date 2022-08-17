ce2fee44782b0ba2da23f080603836c9c513b770
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-17 22:12:49 +0100

    add platform details route logic

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -1,5 +1,8 @@
 // Require Platfrom Model
 const Platform = require("../models/platform");
+const Game = require("../models/game");
+// Require async
+const async = require("async");
 // Display List of all Platforms
 exports.platform_list = (req, res) => {
   Platform.find().exec((err, list_platform) => {
@@ -12,8 +15,33 @@ exports.platform_list = (req, res) => {
   });
 };
 // Display a Detail Page for a Specific Platform
-exports.platform_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Platform Detail ${req.params.id}`);
+exports.platform_detail = (req, res, next) => {
+  async.parallel(
+    {
+      platform: function (callback) {
+        Platform.findById(req.params.id).exec(callback);
+      },
+      platform_games: function (callback) {
+        Game.find({ platform: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+      if (results.platform == null) {
+        // No results.
+        const err = new Error("Platform not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      console.log(results);
+      res.render("platform_detail", {
+        title: "Platform Detail",
+        platform: results.platform,
+        platform_games: results.platform_games,
+      });
+    }
+  );
 };
 
 // Display Create Form on GET
