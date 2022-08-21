715f4c2ac49750ede10d13d723aa6a451d4d1df6
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-21 16:45:36 +0100

    rename  title field to name

--- models/developer.js
+++ models/developer.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const developerSchema = new Schema({
-  title: { type: String, required: true, maxLength: 100, minLength: 3 },
+  name: { type: String, required: true, maxLength: 100, minLength: 3 },
   founded: { type: Number, required: true },
   summary: { type: String, required: true },
   logo: { type: String, required: false },
