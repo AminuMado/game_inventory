24942c32ef306afa71e871825230e9552a176c81
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-08-21 16:45:05 +0100

    add create developer route handler logic

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -4,7 +4,8 @@ const Game = require("../models/game");
 
 // Require async
 const async = require("async");
-
+// Require body and validator
+const { body, validationResult } = require("express-validator");
 // Display list of All Developers
 exports.developer_list = (req, res) => {
   Developer.find().exec((err, list_developer) => {
@@ -50,13 +51,63 @@ exports.developer_detail = (req, res, next) => {
 
 // Display Create Form on GET
 exports.developer_create_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Developer Create GET");
+  res.render("developer_form", { title: "Create Developer" });
 };
 
 // Handle Create on POST
-exports.developer_create_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Developer Create POST");
-};
+exports.developer_create_post = [
+  // Validate and santize the name field.
+  body("name", "Developer name required").trim().isLength({ min: 1 }).escape(),
+  // Validate and sanitize the summary field.
+  body("summary", "Developer summary required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  // Validate and sanitize the founded field.
+  body("founded", "Developer established date is required")
+    .isLength({ min: 4 })
+    .escape(),
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+    // Create a developer object with escaped and trimmed data.
+    const developer = new Developer({
+      name: req.body.name,
+      founded: req.body.founded,
+      summary: req.body.summary,
+      icon: req.body.icon,
+    });
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("developer_form", {
+        title: "Create Developer",
+        developer,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from the form is valid
+      // Check if Developer with same name already exists.
+      Developer.findOne({ name: req.body.name }).exec(
+        (err, found_developer) => {
+          if (err) return next(err);
+          if (found_developer) {
+            // Developer exists, redirect to its detail page.
+            res.redirect(found_developer.url);
+          } else {
+            // Everything is good we can finally save he created developer
+            developer.save((err) => {
+              if (err) return next(err);
+              // Developer saved. Redirect to developer detail page.
+              res.redirect(developer.url);
+            });
+          }
+        }
+      );
+    }
+  },
+];
 
 // Display Delete Form on GET
 exports.developer_delete_get = (req, res) => {
